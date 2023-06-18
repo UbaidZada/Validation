88a43955fca87d285c3b8b932d333712605589d5
Ubaid <ubaidzada213@gmail.com>
2023-06-18 11:08:43 +0500

    done regular expression

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ submitbtn.addEventListener('click', (e) => {
 
     e.preventDefault();
 
-    if (validateName()&& validateEmail() && validatePassword() && validateConfirmPassword()) {
+    if (validateName() && validateEmail() && validatePassword() && validateConfirmPassword()) {
 
         alert("Form Submitted Successfully");
         window.location.href = "index.html";
@@ -23,25 +23,25 @@ function validateName() {
 
     let name = document.getElementById('name').value;
 
-    if(name.length == 0){
+    if (name.length == 0) {
 
-       nameError.innerHTML = "Name is required"   
-       nameError.previousElementSibling.classList.add('fa-xmark');
-       return false;
-
-    }
-
-    if(!name.match(/^[a-zA-Z]*\s{1}[a-zA-Z]*$/)){
-
-nameError.innerHTML = "Write full Name"
-return false; 
+        nameError.innerHTML = "Name is required"
+        nameError.previousElementSibling.classList.add('fa-xmark');
+        return false;
 
     }
 
-nameError.innerHTML = ""
-nameError.previousElementSibling.classList.add('fa-check');
+    if (!name.match(/^[a-zA-Z]*\s{1}[a-zA-Z]*$/)) {
 
-return true;
+        nameError.innerHTML = "Write full Name"
+        return false;
+
+    }
+
+    nameError.innerHTML = ""
+    nameError.previousElementSibling.classList.add('fa-check');
+
+    return true;
 
 }
 
@@ -52,25 +52,25 @@ function validateEmail() {
 
     let email = document.getElementById('email').value;
 
-    if(email.length == 0){
+    if (email.length == 0) {
 
-       emailError.innerHTML = "Email is required"   
-       emailError.previousElementSibling.classList.add('fa-xmark');
-       return false;
-
-    }
-
-    if(!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)){
-
-emailError.innerHTML = "Enter valid email"
-return false; 
+        emailError.innerHTML = "Email is required"
+        emailError.previousElementSibling.classList.add('fa-xmark');
+        return false;
 
     }
 
-emailError.innerHTML = ""
-emailError.previousElementSibling.classList.add('fa-check');
+    if (!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
 
-return true;
+        emailError.innerHTML = "Enter valid email"
+        return false;
+
+    }
+
+    emailError.innerHTML = ""
+    emailError.previousElementSibling.classList.add('fa-check');
+
+    return true;
 
 }
 
@@ -82,29 +82,26 @@ function validatePassword() {
 
     let password = document.getElementById('password').value;
 
-    if(password.length == 0){
+    if (password.length == 0) {
 
-       passError.innerHTML = "Password is required"   
-       passError.previousElementSibling.classList.add('fa-xmark');
-       return false;
+        passError.innerHTML = "Password is required"
+        passError.previousElementSibling.classList.add('fa-xmark');
+        return false;
+
+    }
+
+
+    if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/g)) {
+
+        passError.innerHTML = "Password should contain Minimum eight characters, at least one letter and one number"
+        return false;
 
     }
 
-    
-    if(!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/g)){
+    passError.innerHTML = ""
+    passError.previousElementSibling.classList.add('fa-check');
 
-passError.innerHTML = "Password should contain Minimum eight characters, at least one letter and one number"
-return false; 
-
-    }
-    
-    
-
-
-passError.innerHTML = ""
-passError.previousElementSibling.classList.add('fa-check');
-
-return true;
+    return true;
 
 
 }
@@ -112,47 +109,32 @@ return true;
 // confirm Password
 
 
-function validateConfirmPassword(){
+function validateConfirmPassword() {
     let password = document.getElementById('password').value;
-let confirmpassword = document.getElementById('confirmpassword').value;
+    let confirmpassword = document.getElementById('confirmpassword').value;
 
-if (confirmpassword.length == 0){
+    if (confirmpassword.length == 0) {
 
-confirmpassError.innerHTML = "Confirm Password is required"
-confirmpassError.previousElementSibling.classList.add('fa-xmark');
-return false;
+        confirmpassError.innerHTML = "Confirm Password is required"
+        confirmpassError.previousElementSibling.classList.add('fa-xmark');
+        return false;
+    }
+
+    else if (confirmpassword != password) {
+
+        confirmpassError.innerHTML = "Password should be same"
+        confirmpassError.previousElementSibling.classList.add('fa-xmark');
+        return false;
+    }
+    confirmpassError.innerHTML = "Passsword Matched"
+    confirmpassError.previousElementSibling.classList.add('fa-check');
+
+    return true;
 }
 
-else if(confirmpassword != password){
 
-    confirmpassError.innerHTML = "Password should be same"
-    confirmpassError.previousElementSibling.classList.add('fa-xmark');
-    return false;
-}
-confirmpassError.innerHTML = "Passsword Matched"
-confirmpassError.previousElementSibling.classList.add('fa-check');
+localStorage.setItem("Name1", FullName)
+localStorage.setItem("Email1", Email)
+localStorage.setItem("Password1", Password)
+localStorage.setItem("Cpassword1", ConfirmPassword)
 
-return true;
-}
-
-
-
-// let confirmpassword = document.getElementById('confirmpassword').value;
-// if(confirmpassword.length == 0){
-//     confirmpassError.innerHTML = "Enter Confirm Password"
-//     confirmpassError.previousElementSibling.classList.add('fa-xmark');
-//     return false;  
-// }
-// else if(confirmpassword != password){
-
-//     confirmpassError.innerHTML = "Password should be same"
-//     confirmpassError.previousElementSibling.classList.add('fa-xmark');
-//     return false;
-// }
-// else if(confirmpassword == password){
-
-//     confirmpassError.innerHTML = "Password Matched"
-//     confirmpassError.previousElementSibling.classList.add('fa-check');
-    
-//     return true;
-// }
